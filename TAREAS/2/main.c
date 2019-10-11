018bb78c38be5b85e072269f383f2fa04e26a981
Miranda Gutierrez Celine <celinemgtz17@gmail.com>
2019-10-10 20:38:43 -0500

    anexe ambas tareas

---
+++ TAREAS/2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argu[]){
+	int num;
+	num=atoi(argu[1]);
+	if(num%2==0){
+	printf("1\n");
+	}
+	else{
+		printf("0\n");
+	}
+	return 0;
+}
