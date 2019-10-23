b8c61e02953bafc64cd450098605ea23bcb10c4a
Miranda Gutierrez Celine <celinemgtz17@gmail.com>
2019-10-22 19:45:06 -0500

    Anexo la tarea 3 con corrección

--- TAREAS/3/main.c
+++ TAREAS/3/main.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main (int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
-	float n1,n2;
-	n1=atof(argv[1]);
-	n2=atof(argv[2]);
-	float suma, resta, multiplicación, división;
-	suma=n1+n2;
-	resta=n1-n2;
-	multiplicación=n1*n2;
-	división=n1/n2;
-	printf("\n%.2f",suma);
-	printf("\n%.2f",resta);
-	printf("\n%.2f",multiplicación);
-	printf("\n%.2f\n",división);
+	double x,y;
+	x=atof(argv[1]);
+	y=atof(argv[2]);
+
+	printf("%.2lf\n",x+y );
+	printf("%.2lf\n",x-y );
+	printf("%.2lf\n",x*y );
+	printf("%.2lf\n",x/y );
 
 	return 0;
 }
