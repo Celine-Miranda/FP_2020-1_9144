d54bffcce6e0b0866cc95743d4092336569db9eb
Miranda Gutierrez Celine <celinemgtz17@gmail.com>
2019-10-29 21:18:23 -0600

    agrego modificaciones de tarea 4

--- TAREAS/4/main.c
+++ TAREAS/4/main.c
@@ -1,31 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-	int main (int argc, char* argu[]){
-	//Para guardar la lista de argu se tiene que declarar otra lista
-	//argc hace referencia al numero de argumentos
-	//se declara la variable suma
-	int lista[argc],suma=0;
- 	//Para mayor exactitud tenemos que declarar al promedio como un
- 	//numero real debido a que con esto nuestro resultado lo obtenemos 
-	//con decimales
-	float promedio;
-	//utilizamos for para asi poder copiar argu a la lista
-	//iniciamos con 1
-	for (int i = 1; i < argc; i++){
-	lista[i]= atoi(argu[i]);
+int main(int argc,char*argv[]){
+	//Se declaran variables y valores
+	double x=0;
+
+	//Se utiliza for como estructura de repetición
+	//Se convirten argumentos a flot
+	for(int i=1; i<argc;i++){
+		double y=atof(argv[i]);
+		x=y+x;
 	}
-	//De nuevo utilizamos for pero esta vez para sumar
-	//los elementos que contiene la lista
-	//Como iniciamos con suma=0 entonces la primera ejecucion del
-	//programa será suma=lista[1] y la segunda vez que repita el
-	//proceso será suma=lista[1]+lista[2], esto lo repetirá ciclicamente
-	for (int i = 1; i < argc; i++){
-	suma=suma+lista[i];
-	}
-	//Para obtener el promedio a la suma que realizamos se tiene que
-	//dividir por argc-1 para así poder quitar el ./main y nuestro 
-	//programa sólo tome en cuenta los números que entran
-	promedio=(float)suma/(argc-1);
-	printf("%f\n",promedio);
+	//Se establece la operacion para obtener el promedio
+	double promedio=x/(argc-1);
+	printf("%.2lf\n",promedio);
+
 	return 0;
 }
